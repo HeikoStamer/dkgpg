3c2d5edae1aca4730c27ae834b1af70c62f053df
Heiko Stamer <HeikoStamer@gmx.net>
2017-11-21 20:48:21 +0100

    added stub for new program dkg-keycheck

---
+++ src/dkg-keycheck.cc
@@ -0,0 +1,129 @@
+/*******************************************************************************
+   This file is part of Distributed Privacy Guard (DKGPG).
+
+ Copyright (C) 2017  Heiko Stamer <HeikoStamer@gmx.net>
+
+   DKGPG is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   DKGPG is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with DKGPG; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+*******************************************************************************/
+
+// include headers
+#ifdef HAVE_CONFIG_H
+	#include "dkgpg_config.h"
+#endif
+
+#include <vector>
+#include <map>
+#include <string>
+#include <algorithm>
+
+#include <libTMCG.hh>
+
+#include "dkg-common.hh"
+
+std::vector<std::string>		peers;
+
+std::string				passphrase, userid, ifilename, kfilename;
+tmcg_octets_t				keyid, subkeyid, pub, sub, uidsig, subsig, sec, ssb, uid;
+std::map<size_t, size_t>		idx2dkg, dkg2idx;
+mpz_t					dss_p, dss_q, dss_g, dss_h, dss_x_i, dss_xprime_i, dss_y;
+size_t					dss_n, dss_t, dss_i;
+std::vector<size_t>			dss_qual, dss_x_rvss_qual;
+std::vector< std::vector<mpz_ptr> >	dss_c_ik;
+mpz_t					dkg_p, dkg_q, dkg_g, dkg_h, dkg_x_i, dkg_xprime_i, dkg_y;
+size_t					dkg_n, dkg_t, dkg_i;
+std::vector<size_t>			dkg_qual;
+std::vector<mpz_ptr>			dkg_v_i;
+std::vector< std::vector<mpz_ptr> >	dkg_c_ik;
+gcry_mpi_t 				dsa_p, dsa_q, dsa_g, dsa_y, dsa_x, elg_p, elg_q, elg_g, elg_y, elg_x;
+gcry_mpi_t 				gk, myk, sig_r, sig_s;
+
+int 					opt_verbose = 0;
+
+int main
+	(int argc, char **argv)
+{
+	static const char *usage = "dkg-keycheck [OPTIONS] KEYFILE";
+	static const char *about = PACKAGE_STRING " " PACKAGE_URL;
+	static const char *version = PACKAGE_VERSION " (" PACKAGE_NAME ")";
+
+	// parse command line arguments
+	for (size_t i = 0; i < (size_t)(argc - 1); i++)
+	{
+		std::string arg = argv[i+1];
+		if ((arg.find("--") == 0) || (arg.find("-v") == 0) || (arg.find("-h") == 0) || (arg.find("-V") == 0))
+		{
+			if ((arg.find("-h") == 0) || (arg.find("--help") == 0))
+			{
+				std::cout << usage << std::endl;
+				std::cout << about << std::endl;
+				std::cout << "Arguments mandatory for long options are also mandatory for short options." << std::endl;
+				std::cout << "  -h, --help     print this help" << std::endl;
+				std::cout << "  -v, --version  print the version number" << std::endl;
+				std::cout << "  -V, --verbose  turn on verbose output" << std::endl;
+				return 0; // not continue
+			}
+			if ((arg.find("-v") == 0) || (arg.find("--version") == 0))
+			{
+				std::cout << "dkg-keycheck v" << version << std::endl;
+				return 0; // not continue
+			}
+			if ((arg.find("-V") == 0) || (arg.find("--verbose") == 0))
+				opt_verbose++; // increase verbosity
+			continue;
+		}
+		else if (arg.find("-") == 0)
+		{
+			std::cerr << "ERROR: unknown option \"" << arg << "\"" << std::endl;
+			return -1;
+		}
+		kfilename = arg;
+	}
+
+	// check command line arguments
+	if (kfilename.length() == 0)
+	{
+		std::cerr << "ERROR: argument KEYFILE is missing; usage: " << usage << std::endl;
+		return -1;
+	}
+
+	// initialize LibTMCG
+	if (!init_libTMCG())
+	{
+		std::cerr << "ERROR: initialization of LibTMCG failed" << std::endl;
+		return -1;
+	}
+	if (opt_verbose)
+		std::cout << "INFO: using LibTMCG version " << version_libTMCG() << std::endl;
+
+	// read and parse the public key (no ElGamal subkey required)
+	std::string armored_pubkey;
+	if (!read_key_file(kfilename, armored_pubkey))
+		return -1;
+	init_mpis();
+	time_t ckeytime = 0, ekeytime = 0;
+	if (!parse_public_key(armored_pubkey, ckeytime, ekeytime, false))
+	{
+		std::cerr << "ERROR: cannot parse the provided public key" << std::endl;
+		release_mpis();
+		return -1;
+	}
+
+// TODO
+
+	// release mpis and keys
+	release_mpis();
+	
+	return 0;
+}
